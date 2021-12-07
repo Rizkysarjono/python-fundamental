4a5c208cae0c5443570753c91199d62eae147e73
Rizkysarjono <aquaticapes77@gmail.com>
2021-12-07 15:40:44 +0700

    mom and son

---
+++ main.py
@@ -0,0 +1,21 @@
+print('mom said,"son please go to store and get me 1 bottle of milk and if they had eggs bring 6 eggs"')
+print('son said,"okay mama"')
+print('"son on his way to the store"')
+print('"son arrives at store."')
+amount_of_milk=(0)
+
+if amount_of_milk > 0:
+    print("son buy 1 bottle of milk")
+else:
+    print('son not buy 1 bottle of milk')
+print('"son checking if there is 6 eggs at the store."')
+amount_of_egg=(6)
+
+if amount_of_egg > 6:
+    print("son buy 6 eggs")
+else:
+    print("son not buy 6 eggs")
+
+print('"son go back to home"')
+print('"son arrives at home"')
+print('"son give it all to mom what he got from the store"')
